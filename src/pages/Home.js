36d470ad4fb28ff30a17dd5a382ce990a1f8eb7f
ssing119 <43161218+ssing119@users.noreply.github.com>
2020-12-15 22:07:17 -0800

    link index.html file

---
+++ src/pages/Home.js
@@ -0,0 +1,351 @@
+import React, {Component} from 'react';
+class Home extends Component{
+    render(){
+        return(
+            <div className="row ">
+                <div className="medium-12 columns">
+                <div class="hadder">
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="index.html"><img src="images/logo.svg" alt="LOGO"/></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+<li class="active"><a class="nav-link" href="index.html">Home</a></li>  
+<li><a class="nav-link" href="#">Borrow</a></li>
+<li><a class="nav-link" href="#">Mortage Rate</a></li>
+<li><a class="nav-link" href="#">Easy Apply</a></li>
+<li><a class="nav-link" href="#">FAQ</a></li>
+<li><a class="nav-link" href="#">About US</a></li>
+<li><a class="nav-link" href="#">Contact</a></li> 
+<li><a class="login-btn" href="#">LOGIN</a></li>   
+</ul>  
+</div>
+</nav>
+</div>
+</div>
+
+<div class="banner-saction">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="banner-saction-content">
+<h2>Why Installment Loans Are <br/>
+<strong>Better Than Payday Loans</strong></h2>
+<p>Join a vibrant community of MSPs to forge long-lasting relationships<br/> with partners that help you create excellent customer experiences</p>
+<div class="apply-btn"><a href="step-from.html">Apply Now</a></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="speed-money-saction">
+<div class="container">
+<h2>Why choose Speedy Money</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+<div class="row">
+<div class="col-md-4">
+<div class="speed-money-saction-content">
+<img src="images/High Rang Loan.svg" alt="#"/>
+<span>Up to $5M</span>
+<h4>High Rang Loan</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+<div class="col-md-4">
+<div class="speed-money-saction-content">
+<img src="images/Offer Low Interest.svg" alt="#"/>
+<span>from</span>
+<h4>Offer Low Interest</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+<div class="col-md-4">
+<div class="speed-money-saction-content">
+<img src="images/Easy & Easy Process.svg" alt="#"/>
+<span>10 days Process</span>
+<h4>Easy & Easy Process</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+<div class="about-speed-money-saction">
+<div class="container">
+<div class="col-md-6">
+<div class="about-speed-money-saction-text">
+<h2>About Speedy Money</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+<ul class="about-list">
+<li><img src="images/erow-icon.svg" alt="#" />Approval for bad credit. </li>
+<li><img src="images/erow-icon.svg" alt="#"/>Low interest rates. </li>
+<li><img src="images/erow-icon.svg" alt="#" />Quick cash.</li>
+<li><img src="images/erow-icon.svg" alt="#" />Flexible terms.</li>
+</ul>
+</div>
+</div>
+<div class="col-md-6">
+<div class="about-speed-money-saction-text">
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="do-our-lending-Online-saction">
+<div class="container">
+<div class="do-our-lending-Online-saction-content">
+<div class="row">
+<div class="col-md-12">
+<h2>Do Your Lending Online</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="blue-btn"><a href="http://codlogic.com/devteam/speedy/step-from.html">Apply Now</a></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="easy-loan-process-saction">
+<div class="container">
+<h2>Easy Loan Process</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+<div class="row">
+<div class="col-md-4">
+<div class="easy-loan-process-saction-content">
+<div class="number-box"><span>1</span></div>
+<h4>Reach Out to Us</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+<div class="col-md-4">
+<div class="easy-loan-process-saction-content">
+<div class="number-box"><span>2</span></div>
+<h4>Fill in the form</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+<div class="col-md-4">
+<div class="easy-loan-process-saction-content">
+<div class="number-box"><span>3</span></div>
+<h4>Loan Disbursed</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="customer-feedback-saction">
+<div class="container">
+<h2>Customer Feedback</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+<div class="customer-feedback-saction-content">
+<div class="row">
+<div class="items">
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>           
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>          
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>          
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>          
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>         
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>          
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>         
+</div>
+</div>
+<div class="card">
+<div class="card-body">
+<ul class="star-box">
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+<li><img src="images/star.svg" alt="#"/></li>
+</ul> 
+<div class="date-box">28 november</div>
+<h4>Muhammad</h4>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+More</p>         
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class="frequently-questions-saction">
+<div class="container">
+<h2>Frequently ask Questions</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+<div class="row">
+<div class="faq-saction">
+<div class="accordion" id="accordionExample">
+<div class="card">
+<div class="card-header" id="headingOne">
+<h2 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">How Do I Get A Payday Loan?</button></h2>
+</div>
+<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+<div class="card-body">
+Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officiaaute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+</div>
+</div>
+</div>
+<div class="card">
+<div class="card-header" id="headingTwo">
+<h2 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">What Can I Use My Payday Loan For?</button></h2>
+</div>
+<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+<div class="card-body">
+Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+</div>
+</div>
+</div>
+<div class="card">
+<div class="card-header" id="headingThree">
+<h2 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">How Do I Repay My Loan?</button></h2>
+</div>
+<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+<div class="card-body">
+Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officiaaute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="footer-saction">
+<div class="container">
+<div class="col-md-12">
+<div class="footer-saction-content">
+<h2>Get Up To $1,500 Instantly<a href="http://codlogic.com/devteam/speedy/step-from.html">Apply Now</a></h2>
+</div>
+</div>
+</div>
+<div class="footer-copy-right-saction">
+<p>© 2020 Speedy Money. All rights reserved. </p>
+</div>
+</div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Home;
